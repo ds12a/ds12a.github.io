e1675b99ab0984442b480643d4a4a74538b48844
dshen12 <81164854+dshen12@users.noreply.github.com>
2021-04-15 11:37:36 -0400

    Update particle_config.js

--- js/particle_config.js
+++ js/particle_config.js
@@ -1,3 +1,12 @@
+window.addEventListener("error", handleError, true);
+
+function handleError(evt) {
+    if (evt.message) { // Chrome sometimes provides this
+      alert("error: "+evt.message +" at linenumber: "+evt.lineno+" of file: "+evt.filename);
+    } else {
+      alert("error: "+evt.type+" from element: "+(evt.srcElement || evt.target));
+    }
+}
 particlesJS.load('particles-js', '../assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
