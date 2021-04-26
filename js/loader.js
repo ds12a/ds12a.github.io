f231c981f001ad66206f82403aa9da7e6f644656
dshen12 <81164854+dshen12@users.noreply.github.com>
2021-04-26 10:20:18 -0400

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,15 @@
 var IDs = ["home", "test"];
 function loadPage(file){
+  alert("loadPage");
   for(i of IDs){
+    alert(i);
     if ($('#'+i).hasClass('active')) {
       $('#'+i).removeClass('active');
       break;
     }
   }
   var id = file.slice(0,-4);
+  alert("id: "+id);
   $('#'+id).addClass(' active');
   $("#content").load(file);
 }
