7592b1c2ab3bf118804adde2c93f595960295ede
ds12a <david.yz.shen@gmail.com>
2022-04-27 21:50:00 +0000

    Update

---
+++ ds12a/src/App.js
@@ -0,0 +1,17 @@
+import logo from './images/favicon.ico';
+import './App.css';
+import {NavBar} from './Components/NavBar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <title>Website of ds12a</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href={logo} type="image/x-icon" />
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
